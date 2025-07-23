fa6fb20c2225b577cdc97c17806636b9e625a1d6
hamza iddou <hamzaiddou2004@gmail.com>
2025-07-23 15:22:41 +0100

    start make the search bar automatic

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,22 @@
 import { Button, Form } from "react-bootstrap"
 import styles from "./SearchBar.module.scss"
+import { Autocomplete, TextField } from "@mui/material"
 const SearchBar = () => {
+
+  const handleInputChange = (e) =>{
+      const {value} = e.currentTarget
+      console.log(value)
+  }
+
+
   return (
     <>
     <Form>
         <Form.Group className={`${styles.searchContainer}`}>
-            <Form.Label></Form.Label>
-            <Form.Control className={`${styles.searchInput}`} type="text" size={"lg"} placeholder="Enter Your City ..."></Form.Control>
+          <Autocomplete className={`${styles.searchInput}`} renderInput={(params) =>
+             <TextField  onChange={handleInputChange}   {...params} label={'Enter your City ...'} />} 
+             options={['Casablanca', 'Rabat', 'Tangier']}/>
+            
             <Button variant="primary" size={"sm"}>Search</Button>
         </Form.Group>
         
